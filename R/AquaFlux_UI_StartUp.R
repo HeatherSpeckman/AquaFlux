0411f122f6997eccc133aed363220d31e8b23447
HeatherSpeckman <hspeckma@uwyo.edu>
2019-07-06 12:31:04 -0600

    Update AquaFlux_UI_StartUp.R

--- R/AquaFlux_UI_StartUp.R
+++ R/AquaFlux_UI_StartUp.R
@@ -13,14 +13,11 @@
 #####################################################################
 
 .test.GuiDir = function(a, lab, v){
-<<<<<<< HEAD
-
   print("lilo in .test.GuiDir") #lilo, v
   print(v$setup.SiteType ) #lilo
   print("lilo in .test.GuiDir 2") #lilo, v
 
-=======
->>>>>>> c16a7b0f79316b1e8c33a69e6ad48242b4b564a6
+
   # get the file path
   b = unlist(a[1])
   b0 = paste0(b, collapse = "/")

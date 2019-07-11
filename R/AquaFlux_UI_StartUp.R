94b60652268016ab20eafe244fa313fe23c5447e
Heather Speckman <hspeckma@uwyo.edu>
2019-07-11 17:35:56 -0600

    Update AquaFlux_UI_StartUp.R

--- R/AquaFlux_UI_StartUp.R
+++ R/AquaFlux_UI_StartUp.R
@@ -13,7 +13,7 @@
 
 .test.GuiDir = function(a, lab, v, roots){
   print("")
-  print("new go - lilo4")
+  print("new go - lilo5")
   # get the file path
   b = unlist(a[1])
   b0 = paste0(b, collapse = "/")

4e73a0ee28f939631607fd0747dbdf1dd1d3fd0d
Heather Speckman <hspeckma@uwyo.edu>
2020-05-08 10:21:46 -0600

    Update AquaFlux_ImportFunctions.R

--- R/AquaFlux_ImportFunctions.R
+++ R/AquaFlux_ImportFunctions.R
@@ -127,22 +127,14 @@
       x = d.merge
     }
   }
+  
     #africa
   print("in .import.raw.dT.data 3")
   print(head(d.merge))
-  print(d.merge$TIMESTAMP)
-  x = duplicated(d.merge$TIMESTAMP)
-  print(duplicated(d.merge$TIMESTAMP)[x] )
-  print(sum(x))
   
-  d.merge= d.merge[ duplicated(d.merge$TIMESTAMP)==F, ]
-  
-      #africa
-  print("in .import.raw.dT.data 4")
-  print(head(d.merge))
-  print(d.merge$TIMESTAMP)
+  # deactivated: remove duplicated timestamps
+  #d.merge= d.merge[ duplicated(d.merge$TIMESTAMP)==F, ]
 
-  
   d.merge
 }
 
